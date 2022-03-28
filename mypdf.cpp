fcd1a11fc7a5e5c0993d7f338c03ce701f3b2ce8
Benny Andersen <a.mail.user@gmail.com>
2022-03-28 18:09:19 +0200

    &&->||

--- mypdf.cpp
+++ mypdf.cpp
@@ -377,7 +377,7 @@ int test() {
 
 			
 int main(int argc, char * argv[]) {
-	if (argc < 2 && argc > 3) 
+	if (argc < 2 || argc > 3) 
     	return syntax();
 	switch(getopt(argc, argv, ":a:hst")) 
 	{ 
